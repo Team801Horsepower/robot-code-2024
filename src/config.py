5fc4e73993c8e5f3c020a3e388bd19c70ffbfd5a
Ishaan Sen <ishaan.sen100@gmail.com>
2023-09-11 19:08:37 -0400

    config

--- src/config.py
+++ src/config.py
@@ -1,21 +1,22 @@
 import wpimath
 from wpimath import units
 from wpimath.geometry import Translation2d
+import tomllib
 
-
-# lengths: meters
+with open('/config.toml', 'rb') as f:
+    robot_data = tomllib.load(f)['robot']
 
 # (length, width)
-robot_dimensions = Translation2d(0.631825, 0.53975)
+# robot_dimensions = Translation2d(0.631825, 0.53975)
 
 # Old Chassis
-drive_gear_ratio = 16.0 / 3.0
-turn_gear_ratio = 60.0
+# drive_gear_ratio = 16.0 / 3.0
+# turn_gear_ratio = 60.0
 # New Chassis
 # drive_gear_ratio = 6.0
 # turn_gear_ratio = 25.5
 
-wheel_diameter = units.inchesToMeters(4.0)
+# wheel_diameter = units.inchesToMeters(4.0)
 
 drive_speed = 150.0
 turn_speed = 150.0
--- src/subsystems/drive.py
+++ src/subsystems/drive.py
@@ -13,7 +13,7 @@ from itertools import chain
 
 from typing import List
 
-import config
+from config import robot_data
 
 # pylint: disable=missing-docstring, too-few-public-methods
 class Drive:
@@ -24,8 +24,18 @@ class Drive:
                 CANSparkMax(turn_id, CANSparkMax.MotorType.kBrushless),
             )
 
-        self.swerves_l = list(map(lambda t: make_swerve(*t), [(12, 11), (20, 19)]))
-        self.swerves_r = list(map(lambda t: make_swerve(*t), [(9, 10), (2, 1)]))
+        self.turn_gear_ratio = robot_data['drive']['gear_ratios']['turn']
+        self.drive_gear_ratio = robot_data['drive']['gear_ratios']['drive']
+        
+        self.wheel_diameter = robot_data['drive']['wheel_dia']
+        
+        self.robot_dimensions = Translation2d(*robot_data['chassis']['dimensions'])
+
+        self.swerves_l = list(map(lambda t: make_swerve(*t), robot_data['drive']['swerves_l']))
+        self.swerves_r = list(map(lambda t: make_swerve(*t), robot_data['drive']['swerves_r']))
+
+        # self.swerves_l = list(map(lambda t: make_swerve(*t), [(12, 11), (20, 19)]))
+        # self.swerves_r = list(map(lambda t: make_swerve(*t), [(9, 10), (2, 1)]))
 
         for swerve in chain(self.swerves_l, self.swerves_r):
             swerve.drive_encoder.setPosition(0.0)
@@ -38,22 +48,27 @@ class Drive:
     def set_swerves(self):
         for swerve in chain(self.swerves_l, self.swerves_r):
             cur_position = swerve.turn_encoder.getPosition()
-            half_turn = config.turn_gear_ratio / 2.0
+            half_turn = self.turn_gear_ratio / 2.0
             zero_position = 0.0
             while zero_position < cur_position - half_turn:
                 zero_position += 2.0 * half_turn
             while zero_position > cur_position + half_turn:
                 zero_position -= 2.0 * half_turn
-            swerve.turn_pid.setReference(zero_position, rev.CANSparkMaxLowLevel.ControlType.kPosition)
+            swerve.turn_pid.setReference(
+                zero_position, rev.CANSparkMaxLowLevel.ControlType.kPosition
+            )
 
     """
     `vel`: (forward, leftward, counterclockwise), m/s, rad/s
     """
+
     def drive(self, vel: Transform2d) -> None:
         # Equality check is fine here since the deadzone handles rounding to 0.
         if vel.rotation().degrees() == 0.0 and vel.translation().norm() == 0.0:
             for swerve in chain(self.swerves_l, self.swerves_r):
-                swerve.drive_pid.setReference(0.0, rev.CANSparkMaxLowLevel.ControlType.kVelocity)
+                swerve.drive_pid.setReference(
+                    0.0, rev.CANSparkMaxLowLevel.ControlType.kVelocity
+                )
             return
 
         swerves = zip(
@@ -64,17 +79,21 @@ class Drive:
             # Normalizing involves dividing by (radius * 2), but converting from
             # angular velocity to linear velocity means multiplying by radius,
             # leaving only a division by 2.
-            rot_vec = config.robot_dimensions / 2.0 * vel.rotation().radians()
+            rot_vec = self.robot_dimensions / 2.0 * vel.rotation().radians()
             rot_vec = Translation2d(rot_vec.x * pos[0], rot_vec.y * pos[1])
             rot_vec = rot_vec.rotateBy(Rotation2d.fromDegrees(90.0))
 
             total_vec = rot_vec + vel.translation()
 
-            turn_position = total_vec.angle().degrees() / 360.0 * config.turn_gear_ratio
-            drive_speed = total_vec.norm() / (pi * config.wheel_diameter) * config.drive_gear_ratio
+            turn_position = total_vec.angle().degrees() / 360.0 * self.turn_gear_ratio
+            drive_speed = (
+                total_vec.norm()
+                / (pi * self.wheel_diameter)
+                * self.drive_gear_ratio
+            )
 
             cur_position = swerve.turn_encoder.getPosition()
-            half_turn = config.turn_gear_ratio / 2.0
+            half_turn = self.turn_gear_ratio / 2.0
 
             while turn_position < cur_position - half_turn:
                 turn_position += 2.0 * half_turn
