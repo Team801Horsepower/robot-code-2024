e30c2942f9483c326f4e051eba6e12bf5b1d61a8
Ishaan Sen <ishaan.sen100@gmail.com>
2024-01-13 10:46:41 -0500

    Add batch script for Aneesha to test

---
+++ frc.bat
@@ -0,0 +1,28 @@
+@echo off
+
+:frc_help
+echo Usage: frc.cmd [verify ^| deploy ^| console ^| shell ^| rlds]
+goto :eof
+
+if "%1" == "" (
+    call :frc_help
+) else if "%1" == "verify" (
+    echo Running black auto-formatter...
+    poetry run black src/
+    echo Running mypy static type checker...
+    poetry run mypy src/
+    echo Running pylint linter...
+    poetry run pylint src/ --disable=missing-docstring,invalid-name,missing-module-docstring
+) else if "%1" == "deploy" (
+    cd src/
+    poetry run robotpy deploy --skip-tests --team 801
+    cd ..
+) else if "%1" == "console" (
+    poetry run netconsole 10.8.1.2
+) else if "%1" == "shell" (
+    poetry shell
+) else if "%1" == "rlds" (
+    ssh -o StrictHostKeyChecking=no admin@10.8.1.2 'sh /home/lvuser/rlds/deploy'
+) else (
+    call :frc_help
+)
