dd77b2923ed095db749ade9737d38cc2876cd154
aneeshaguna <55953635+aneeshaguna@users.noreply.github.com>
2024-01-24 18:35:35 -0500

    modified batch

--- frc.bat
+++ frc.bat
@@ -1,22 +1,19 @@
 @echo off
 
-:frc_help
-echo Usage: frc.cmd [verify ^| deploy ^| console ^| shell ^| rlds]
-goto :eof
-
 if "%1" == "" (
-    call :frc_help
+    echo Usage: frc.bat [verify | deploy | console]
 ) else if "%1" == "verify" (
     echo Running black auto-formatter...
     poetry run black src/
+    echo.
     echo Running mypy static type checker...
     poetry run mypy src/
+    echo.
     echo Running pylint linter...
     poetry run pylint src/ --disable=missing-docstring,invalid-name,missing-module-docstring
 ) else if "%1" == "deploy" (
     cd src/
     poetry run robotpy deploy --skip-tests --team 801
-    cd ..
 ) else if "%1" == "console" (
     poetry run netconsole 10.8.1.2
 ) else if "%1" == "shell" (
@@ -24,5 +21,5 @@ if "%1" == "" (
 ) else if "%1" == "rlds" (
     ssh -o StrictHostKeyChecking=no admin@10.8.1.2 'sh /home/lvuser/rlds/deploy'
 ) else (
-    call :frc_help
+    echo Usage: frc.bat [verify | deploy | console]
 )
