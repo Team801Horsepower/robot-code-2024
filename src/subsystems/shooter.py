8dcae7b677c2adc13ebbfdf5acd222d5ae619445
Ishaan Sen <ishaan.sen100@gmail.com>
2024-02-03 16:04:23 -0500

    Add shooter pitch control

--- src/subsystems/shooter.py
+++ src/subsystems/shooter.py
@@ -1,7 +1,7 @@
 from typing import List
 from rev import CANSparkMax, SparkPIDController
 from wpilib import DutyCycleEncoder
-
+from math import pi
 
 class Shooter:
     def __init__(self, flywheel_motors: List[int], feeder_motor: int, pitch_motor: int):
@@ -27,3 +27,24 @@ class Shooter:
     def start_flywheels(self, speed: float) -> None:
         self.flywheel_pids[0].setReference(speed, CANSparkMax.ControlType.kVelocity)
         self.flywheel_pids[1].setReference(-speed, CANSparkMax.ControlType.kVelocity)
+
+    def get_pitch(self) -> float:
+        angle_offset = 0
+        angle = self.pitch_encoder.get() * 2.0 * pi + angle_offset
+
+        while angle > pi:
+            angle -= 2.0 * pi
+
+        while angle < pi:
+            angle += 2.0 * pi
+
+        return angle
+
+    def seek_pitch(self, target_pitch: float) -> None:
+        current_pitch = get_pitch()
+        if abs(current_pitch - target_pitch) < 0.05:
+            self.pitch_motor.set(0)
+        else if current_pitch > target_pitch:
+            self.pitch_motor.set(0.1)
+        else if current_pitch < target_pitch:
+            self.pitch_motor.set(-0.1)
