186ee2258d2bdb5e42f969a90d3be8f989912afd
Ishaan Sen <ishaan.sen100@gmail.com>
2024-03-30 14:49:03 -0400

    Add note vision

---
+++ src/subsystems/note_vision.py
@@ -0,0 +1,38 @@
+import math
+from operator import itemgetter
+from photonlibpy.photonCamera import PhotonCamera
+
+from commands2 import Subsystem, CommandScheduler
+
+
+from config import rear_camera_angle_offset, second_camera_height
+
+
+class Vision(Subsystem):
+    def __init__(self, scheduler: CommandScheduler, camera_name="Camera_Module_v2"):
+        self.camera = PhotonCamera(camera_name)
+        scheduler.registerSubsystem(self)
+    
+    def periodic(self):
+        print(self.get_distance_degree())
+
+    def get_distance_degree(self) -> dict[float, float] | None:
+        result = self.camera.getLatestResult()
+        # note = {"dist": value, "angle": value}
+        all_distances = []
+        if len(result.getTargets()) >= 1:
+            for target in result.getTargets():
+                target_to_cam_angle = target.getPitch()
+                distance = second_camera_height / (
+                    math.tan(math.radians(target_to_cam_angle))
+                )
+                angle = target.getYaw()
+                final_angle = angle + rear_camera_angle_offset
+                all_distances.append({"dist": distance, "angle": final_angle})
+
+            final_distances = sorted(all_distances, key=itemgetter("dist"))
+            closest_note = final_distances[0]
+            return closest_note
+
+        else:
+            return None
